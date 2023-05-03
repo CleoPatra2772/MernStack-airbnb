291a57782d7d0e615d1624d5c06a2fd15e7fe395
Cleo Gao <celeo127@outlook.com>
2023-05-03 19:45:49 +0800

    ui for login and register page

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+
+import Header from "../components/Header";
+const IndexPage = () => {
+    return(
+        <div>
+            This is index page
+        </div>
+    )
+}
+
+export default IndexPage;
